94807b410acdec0638a816c24bf33bd546711d23
Josh Daniels <daniels.joshua100@gmail.com>
2014-07-18 18:41:08 -0400

    added more bitters and styled

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,20 @@
 // console.log('The Iron Yard Rocks');
 // console.log(Backbone);
 // console.log(Handlebars);
+   $(document).ready(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+}); 
+
     $(function() {
         $('#edit').editable({inlineMode: false})
     });
